17ae02130c3e09560362bfc1fb660eb010b368f1
tristeceratops <eric.woillard@gmail.com>
2024-05-24 17:00:02 +0200

    test thread and mutex

--- utils.c
+++ utils.c
@@ -1,34 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ewoillar <ewoillar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/24 14:19:41 by ewoillar          #+#    #+#             */
-/*   Updated: 2024/05/24 16:59:37 by ewoillar         ###   ########.fr       */
+/*   Created: 2024/05/24 15:01:00 by ewoillar          #+#    #+#             */
+/*   Updated: 2024/05/24 15:03:41 by ewoillar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
-# include <sys/time.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <pthread.h>
+#include "philo.h"
 
-typedef struct s_philo
+void    ft_putchar(int fd, char *c)
 {
-	int	philo_numbers;
-	int	time_die;
-	int	time_eat;
-	int time_sleep;
-	int	rounds;
-	int	isalive;
-	pthread_mutex_t *fork_l;
-	pthread_mutex_t *fork_r;
-}	t_philo;
+    write(1, c, 1);
+}
 
-void    ft_putstr(int fd, char *str);
-#endif
+void    ft_putstr(int fd, char *str)
+{
+    while (*str)
+    {
+        ft_putchar(1, str);
+        str++;
+    }
+}
